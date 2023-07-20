af236cc9a44900ae2adf65459226aea67b7aecc3
biel-igl <gabrielpires524@gmail.com>
2023-07-20 18:09:25 -0300

    feat: inicia o projeto e faz o req 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingrediente = Ingredient("ovo")
+
+    assert ingrediente.__repr__() == "Ingredient('ovo')"
+    assert ingrediente.__hash__() == hash("ovo")
+    assert ingrediente.name == "ovo"
+    assert ingrediente.restrictions
+    assert ingrediente != 'oi'
+    assert ingrediente != "ovo"
+    assert ingrediente == Ingredient("ovo")
